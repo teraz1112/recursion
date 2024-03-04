76daa5d86d0476e43f41a9853ddb20dbd0eae040
teraz1112 <terazono.daigo.p4@dc.tohoku.ac.jp>
2024-03-04 20:12:01 +0900

    Refactor client.py and remove server.py

--- backendProject/2/remoteProcedureCall/srcs/client.py
+++ backendProject/2/remoteProcedureCall/srcs/client.py
@@ -2,3 +2,58 @@ import json
 import socket
 import sys
 
+class Client:
+    def __init__(self, server_address):
+        self.server_address = server_address
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+    def connect(self):
+        server_address=self.server_address
+        print('connecting to {}'.format(server_address))
+    
+        try:
+            self.sock.connect(server_address)
+        except socket.error as msg:
+            print(msg)
+            sys.exit(1)
+
+    def send(self):
+        while True:
+            try:
+                print('sending message')
+                with open('../json/sample2.json', 'r') as message:
+                    if message:
+                        message_dict = json.load(message)
+                        self.sock.sendall(json.dumps(message_dict).encode('utf-8'))
+                    else:
+                        print('no message')
+                        break
+
+                response = self.sock.recv(4096).decode('utf-8')
+                if response:
+                    print(response)
+                else:
+                    print('no data from', self.server_address)
+                    break
+            except FileNotFoundError:
+                print("The file was not found")
+                break
+            except json.JSONDecodeError:
+                print("Could not decode the JSON file")
+                break
+            except socket.error as e:
+                print("Socket error:", e)
+                break
+            finally:
+                print('closing socket')
+                self.sock.close()
+                sys.exit()
+                break
+            
+    def start(self):
+        self.connect()
+        self.send()
+
+if __name__ == '__main__':
+    client = Client('socket_file')
+    client.start()
--- backendProject/2/remoteProcedureCall/srcs/server.py
+++ backendProject/2/remoteProcedureCall/srcs/server.py
@@ -33,7 +33,7 @@ class Process:
     
     @staticmethod
     def reverse(params):
-        return params[::-1]
+        return params[0][::-1]
     
     @staticmethod
     def validAnagram(params):
@@ -52,14 +52,16 @@ class Server:
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     
     def connect(self):
+        sock=self.sock
+
         try:
             os.unlink(self.server_address)
         except FileNotFoundError:
             pass
 
         print('starting up on {}'.format(self.server_address))
-        self.sock.bind(self.server_address)
-        self.sock.listen(1)
+        sock.bind(self.server_address)
+        sock.listen(1)
     
     def listen(self):
         while True:
@@ -67,5 +69,40 @@ class Server:
             try:
                 print('connection from'+str(client_address))
 
-        
+                self.handle(connection, client_address)
+            finally:
+                connection.close()
+    
+    def handle(self, connection, client_address):
+        while True:
+            data = connection.recv(4096).decode('utf-8')
+            if data:
+                data_dict = json.load(data)
+                print('received {!r}'.format(data_dict))
+
+                method = data_dict['method']
+                params = data_dict['params']
+                id = data_dict['id']
+
+                result = Process.process_method(method, params)
+                result_dict = {}
+                result_dict['result'] = result
+                result_dict['result_type'] = type(result).__name__
+                result_dict['id'] = id
+
+                result_json = json.dumps(result_dict)
+                connection.sendall(result_json.encode('utf-8'))
+                print('sent {!r}'.format(result_dict))
+
+            else:
+                print('no data from', client_address)
+                break
+    
+    def start(self):
+        self.connect()
+        self.listen()
+                
+if __name__ == '__main__':
+    server = Server('socket_file')
+    server.start()
         
